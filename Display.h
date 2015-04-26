e96dc4a2eb25a21b8d0da71a8cb3bde616ef981f
VictorPizzurke <rassledovaniye@gmail.com>
2015-04-27 00:38:17 +0300

    Revert "Delete Display.h"
    This reverts commit ee4e4f75b73efcd90d47c6f3fde385fe3c009067.

---
+++ Display.h
@@ -0,0 +1,37 @@
+#ifndef DISPLAY_H
+#define	DISPLAY_H
+
+#include "IDisplay.h"
+#include <cstdlib>
+#include <iostream>
+#include <sstream>
+//Hello GitHub!
+class Display: public IDisplay
+{
+public:
+    Display();
+    ~Display();
+    virtual void ClearScreen();
+    virtual void Greetings(); // when application starts
+    virtual void InformWinner(std::string winnerName);
+    virtual void InformLooser(std::string looserName);
+    virtual std::string AskForString(std::string availableChars = ""); //whenever a string required from user
+    virtual void AskForSmth();
+    virtual std::string GetMovement();
+    virtual void Message(std::string);
+    virtual int MenuChoose(std::string caption, std::vector<std::string> menu);
+    virtual void ShowField(const IDataTransfer&);
+    virtual void ShowScore(std::map<std::string, int>);
+    virtual void SpecializeDisplay(IDisplay * pDisplay_);
+    virtual bool ExitProgCheckBl();
+protected:
+    virtual std::string GetFiltratedStr(std::string availableChars, int symbolsQuantity = -1);
+    virtual bool CheckAvailableChar(char ch, std::string availableChars);
+    virtual bool CheckPositionInsideLimits(int pos, int downLimit, int upLimit);
+private:
+    IDisplay * pDisplay;
+    std::string NumAllowableStr;
+    std::string LettersAllowableStr;
+};
+
+#endif
